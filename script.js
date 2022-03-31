aa7c461b078ed7b3ac590a48217adab45f5894ff
ygormuller <ygorao80@gmail.com>
2022-03-31 20:27:40 -0300

    add side menu

--- script.js
+++ script.js
@@ -1,82 +1,101 @@
 const newTaskForm = document.getElementById('new-task-form');
 const tasksList = document.getElementById('tasks-list');
+const newTaskPopUp = document.getElementById('new-task-popup');
+const sideMenu = document.getElementById('side-menu');
 
 let tasks = [];
+let showNewTaskPopUp = false;
+let showSideMenu = false;
 
 const clearListView = () => {
-  const children = [...tasksList.children]; // "..." = spread ECS6 converter array de children
+  const children = [...tasksList.children];
   children.forEach((child) => {
     tasksList.removeChild(child);
   });
 }
 
 const updateListView = () => {
-  console.log('updateListView', tasks);
   clearListView();
 
-  //appendChild
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
-    listItem.textContent = task.title;
+    listItem.classList.add('tasks-list-item');
+
+    const header = document.createElement('div');
+    header.classList.add('tasks-list-header');
+    listItem.appendChild(header);
+
+    const checkboxIsDone = document.createElement('input');
+    checkboxIsDone.setAttribute('type', 'checkbox');
+    checkboxIsDone.checked = task.isDone;
+    checkboxIsDone.onchange = () => {
+      handleCheckboxChange(task);
+    }
+    header.appendChild(checkboxIsDone);
+
+    const title = document.createElement('p');
+    title.textContent = task.title;
+    header.appendChild(title);
 
     const buttonDelete = document.createElement('button');
-    buttonDelete.innerHTML = 'Delete'; // não usar inner!!! fácil de hackear
+    buttonDelete.classList.add('button-delete');
+    buttonDelete.innerHTML = 'Excluir';
     buttonDelete.onclick = () => {
-      handleDeleteClick(task)
+      handleDeleteClick(task);
     };
     listItem.appendChild(buttonDelete);
 
-    const checkBoxIsDone = document.createElement('input');
-    checkBoxIsDone.setAttribute('type', 'checkbox');
-    checkBoxIsDone.checked = task.isDone;
-    checkBoxIsDone.onchange = () => {
-      handleCheckBoxChange(task) //atualiza lista
-    }
-    listItem.appendChild(checkBoxIsDone);
-
     tasksList.appendChild(listItem);
-  })
-
-
+  });
+}
 
 const loadFromLocalStorage = () => {
   const savedData = localStorage.getItem('tasks');
 
-    if(savedData === null){
+  if (savedData === null) {
     return;
   }
 
   const parsedData = JSON.parse(savedData);
-    tasks = parsedData;
-  }
+  tasks = parsedData;
+
+  updateListView();
+}
 
 loadFromLocalStorage();
 
 const saveToLocalStorage = () => {
-  const parsedData = JSON.stringify(tasks); //transforma array em JSON
+  const parsedData = JSON.stringify(tasks);
   localStorage.setItem('tasks', parsedData);
 }
 
 const handleDeleteClick = (targetTask) => {
   const filtered = tasks.filter((task) => {
-  return task != targetTask;
+    return task != targetTask;
   });
   tasks = filtered;
 
   saveToLocalStorage();
   updateListView();
-  }
-
-  const handleCheckBoxChange = (targetTask) => {
-    targetTask.isDone = !targetTask.isDone;
-
-    saveToLocalStorage();
-    updateListView();
-  }
-
 }
 
-//constante para callback
+const handleCheckboxChange = (targetTask) => {
+  targetTask.isDone = !targetTask.isDone;
+
+  saveToLocalStorage();
+  updateListView();
+}
+
+const toggleNewTaskPopUp = () => {
+  showNewTaskPopUp = !showNewTaskPopUp;
+
+  if (showNewTaskPopUp) {
+    newTaskPopUp.style.display = 'block';
+  } else {
+    newTaskPopUp.style.display = 'none';
+  }
+}
+
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -90,11 +109,41 @@ const handleSubmit = (event) => {
     isDone: false
   }
 
-  tasks.push(newTask);
+  tasks.unshift(newTask);
   saveToLocalStorage();
 
+  newTaskForm.reset();
+
   updateListView();
+
+  toggleNewTaskPopUp();
 }
 
-//escuntando o formulario
-newTaskForm.addEventListener('submit', handleSubmit)//callback, segundo parametro//
+newTaskForm.addEventListener('submit', handleSubmit);
+
+const handleKeyDown = (event) => {
+  // ESC
+  if (event.keyCode === 27 && showNewTaskPopUp) {
+    toggleNewTaskPopUp();
+  }
+}
+
+document.addEventListener('keydown', handleKeyDown);
+
+const toggleSideMenu = () => {
+  showSideMenu = !showSideMenu;
+
+  if (showSideMenu) {
+    sideMenu.classList.add('side-menu-open');
+  } else {
+    sideMenu.classList.remove('side-menu-open');
+  }
+}
+
+const handleResize = () => {
+  if (window.innerWidth >= 922 && showSideMenu) {
+    toggleSideMenu();
+  }
+}
+
+window.addEventListener('resize', handleResize);
